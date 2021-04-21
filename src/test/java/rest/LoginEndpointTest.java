48c2c7e4cbefa6f96b7f687eea4e039b8ee145ac
Dyrhoi <realarcoz@gmail.com>
2021-04-21 21:06:09 +0200

    Populate unittest.

--- src/test/java/rest/LoginEndpointTest.java
+++ src/test/java/rest/LoginEndpointTest.java
@@ -59,7 +59,6 @@ public class LoginEndpointTest {
     }
 
     // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
